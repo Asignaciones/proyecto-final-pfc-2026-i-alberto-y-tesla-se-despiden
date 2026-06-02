1a53ba1c1f55f6c33a99ba960e2365fc3531dc57
Kevin Andres Rosero Romo <rosero.kevin@correounivalle.edu.co>
2026-06-02 16:10:38 -0500

    Paralelización de movilidad, desperdicio y choques

--- app/src/main/scala/proyecto/AsignacionAulasPar.scala
+++ app/src/main/scala/proyecto/AsignacionAulasPar.scala
@@ -6,14 +6,71 @@ import AsignacionAulas._
 object AsignacionAulasPar {
 
   /** Versión paralela de choques: divide el vector de cursos en dos mitades. */
-  def choquesPar(cursos: Cursos, a: Asignacion): Int = ???
+  def choquesPar(cursos: Cursos, a: Asignacion): Int = {
+    val mid = cursos.length / 2
+    val indicesIzq = (0 until mid).toVector
+    val indicesDer = (mid until cursos.length).toVector
+
+    val paresIzq = indicesIzq.flatMap { i =>
+      (0 until cursos.length).toVector.filter(j => j > i).map(j => (i, j))
+    }
+    val paresDer = indicesDer.flatMap { i =>
+      (0 until cursos.length).toVector.filter(j => j > i).map(j => (i, j))
+    }
+
+    val (choquesIzq, choquesDer) = parallel(
+      paresIzq.count { case (i, j) =>
+        a(i) >= 0 && a(i) == a(j) && solapan(cursos(i), cursos(j))
+      },
+      paresDer.count { case (i, j) =>
+        a(i) >= 0 && a(i) == a(j) && solapan(cursos(i), cursos(j))
+      }
+    )
+    choquesIzq + choquesDer
+  }
 
   /** Versión paralela de desperdicio: divide el vector de cursos en dos mitades. */
-  def desperdicioPar(cursos: Cursos, aulas: Aulas, a: Asignacion): Int = ???
+  def desperdicioPar(cursos: Cursos, aulas: Aulas, a: Asignacion): Int = {
+    val mid = cursos.length / 2
+    val indicesIzq = (0 until mid).toVector
+    val indicesDer = (mid until cursos.length).toVector
+
+    val (desperdicioIzq, desperdicioDer) = parallel(
+      indicesIzq.map { i =>
+        if (a(i) >= 0 && capAula(aulas(a(i))) >= estCurso(cursos(i)))
+          capAula(aulas(a(i))) - estCurso(cursos(i))
+        else 0
+      }.sum,
+      indicesDer.map { i =>
+        if (a(i) >= 0 && capAula(aulas(a(i))) >= estCurso(cursos(i)))
+          capAula(aulas(a(i))) - estCurso(cursos(i))
+        else 0
+      }.sum
+    )
+    desperdicioIzq + desperdicioDer
+  }
+
 
   /** Versión paralela de movilidad: divide el vector de cursos en dos mitades. */
   def movilidadPar(cursos: Cursos, aulas: Aulas, d: Distancias,
-                   a: Asignacion): Int = ???
+                   a: Asignacion): Int = {
+    val asignados = cursos.indices.toVector
+      .filter(i => a(i) >= 0)
+      .sortBy(i => iniCurso(cursos(i)))
+    val mid = asignados.length / 2
+    val consIzq = asignados.take(mid).zip(asignados.take(mid).drop(1))
+    val consDer = asignados.drop(mid).zip(asignados.drop(mid).drop(1))
+
+    val (movIzq, movDer) = parallel(
+      consIzq.map { case (i, j) => d(a(i))(a(j)) }.sum,
+      consDer.map { case (i, j) => d(a(i))(a(j)) }.sum
+    )
+    movIzq + movDer + (
+      if (mid > 0 && mid < asignados.length)
+        d(a(asignados(mid - 1)))(a(asignados(mid)))
+      else 0
+      )
+  }
 
   /**
    * Versión paralela de generarAsignaciones:
